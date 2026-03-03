8a843e7cf597ce5318ddb747a5e8725ed82bc82e
Cyrus Wang <cyruswang@rocketmail.com>
2026-03-03 18:32:25 -0500

    mod 6

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Front-End-Web-App-Development
+
+## Start Web Server
+`python3 -m http.server 8000`
--- mod6_solution/assignment1-starter-code/js/app.js
+++ mod6_solution/assignment1-starter-code/js/app.js
@@ -7,6 +7,23 @@ angular.module('LunchCheck', [])
 LunchCheckController.inject = ['$scope'];
 
 function LunchCheckController ($scope) {
+    $scope.lunchMenu = "";
+    $scope.checkLunch = function () {
+        if ($scope.lunchMenu.trim() === "") {
+            $scope.message = "Please enter data first";
+            $scope.messageColor = 'red';
+        } else {
+            var dishes = $scope.lunchMenu.split(',').filter(function (dish) {
+                return dish.trim() !== "";
+            });
+            if (dishes.length <= 3) {
+                $scope.message = "Enjoy!";
+            } else {
+                $scope.message = "Too much!";
+            }
+            $scope.messageColor = 'green';
+        }
+    };
   
 }
 
